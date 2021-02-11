d8c51a7e67e44af919590fe0ace37d7135d887f1
Robbe <robbe.raevens@student.howest.be>
2021-02-11 15:57:45 +0100

    push alles tot aan de pauze

---
+++ Controllers/WineController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using Labo1Oefening1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Labo1Oefening1.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class WineController : ControllerBase
+    {
+        private static List<Wine> _wines;
+
+        public WineController()
+        {
+            if (_wines == null)
+            {
+                _wines = new List<Wine>();
+                _wines.Add(new Wine(){
+                    WineId = 1, Name = "Barolo", Country = "Italy", Price = 35, Color = "Red", Year = 2007, Grapes = "Nebiollo"
+                });
+
+                _wines.Add(new Wine(){
+                    WineId = 2, Name = "Riesling", Country = "Germany", Price = 35, Color = "White", Year = 2018, Grapes = "Riesling"
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("wines")]
+        public ActionResult<List<Wine>> GetWines(){
+            return new OkObjectResult(_wines);
+        }
+
+        [HttpGet]
+        [Route("wine/{wineId}")]
+        public ActionResult<Wine> GetWine(int wineId){
+            var wine = _wines.Where(w => w.WineId == wineId).SingleOrDefault();
+            if (wine == null) {
+                return new NotFoundObjectResult(wineId);
+            }
+            else {
+                return new OkObjectResult(wine);
+            }
+        }
+            
+    }
+}
---
+++ Models/Wine.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Labo1Oefening1.Models
+{
+    public class Wine
+    {
+        public int WineId { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string Grapes { get; set; }
+    }
+}
